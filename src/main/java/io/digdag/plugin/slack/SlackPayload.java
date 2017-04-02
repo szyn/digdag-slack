b65ce718ff86b68e146ff5ee302a2ab52f6042a3
Shota Suzuki <aqr.aqua@gmail.com>
2017-04-03 07:17:51 +0900

    first commit

---
+++ src/main/java/io/digdag/plugin/slack/SlackPayload.java
@@ -0,0 +1,17 @@
+package io.digdag.plugin.slack;
+
+import org.json.JSONObject;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Map;
+
+public class SlackPayload {
+    public static String convertToJson(String yamlString) {
+        Yaml yaml= new Yaml();
+        Map<String,Object> map= (Map<String, Object>) yaml.load(yamlString);
+
+        JSONObject jsonObject=new JSONObject(map);
+        return jsonObject.toString();
+    }
+
+}
